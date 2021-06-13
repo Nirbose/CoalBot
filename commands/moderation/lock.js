b942355c50d3e0e327e1e7197162a0646547482c
Nirbose <nirbose2@gmail.com>
2021-06-13 12:44:00 +0200

    Moderation commande finish

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -4,6 +4,9 @@ module.exports = {
     categorie: "Moderation",
     execute(message) {
 
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Vous n'avez pas la permition `MANAGE_CHANNELS`.");
+        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Je n'ais pas la permition `MANAGE_CHANNELS`.");
+
         const role = guild.roles.find("name", "Coal");
 
         message.channel.overwritePermissions(role,{ 'SEND_MESSAGES': false })
--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -5,6 +5,9 @@ module.exports = {
     description: "Commande pour débloquer un salon textuel.",
     categorie: "Moderation",
     execute(message) {
+
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Vous n'avez pas la permition `MANAGE_CHANNELS`.");
+        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Je n'ais pas la permition `MANAGE_CHANNELS`.");
         
         const role = guild.roles.find("name", "Coal");
 
